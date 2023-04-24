78f4cd33cf11e092bfe9124038494e0dff17ee82
Pranav Shukla <112813390+pranavshuklaa@users.noreply.github.com>
2023-04-24 19:04:38 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -1,3 +1,29 @@
-alert("padh le chomu " + window.location.href)
-const1 = window.location.href
-console.log(const1)
+
+console.log("Social Media Blocker")
+chrome.runtime.sendMessage({ action: "getTabUrl" }, function(response) {
+  var url = response.url;
+  console.log("Received tab URL from background script: " + url);
+
+  fetch('http://localhost:9000/extract_images', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        url: url
+    }),
+    mode: 'cors'
+})
+
+  .then(response => response.json())
+  .then(data => {
+    
+    console.log(data.image_urls);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
+
+
+
